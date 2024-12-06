43df6bc9ff4a3638297fda7fbefafed9f6ba1b1d
sumpumm <samarpan69.piya@gmail.com>
2024-12-06 16:45:56 +0545

    Chunking data

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-import pdfplumber
 from tkinter.filedialog import askopenfilename
-
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 pdf_path = askopenfilename(filetypes=[("PDF files", "*.pdf")])
 
-with pdfplumber.open('manual.pdf') as pdf:
-    number_pages=len(pdf.pages)
-    print(number_pages)
+def load_documents():
+    loader=PyPDFLoader(pdf_path)
+    return loader.load()
+
+
+
+def split(documents):
+    splitter_object=RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=50,length_function=len)
     
-    pages=pdf.pages
+    chunks=[]
     
-    text=pages[7].extract_text()
-    
-    
-    lines = text.split('\n')
-    for i in range(10):
-        print(lines[i])
-        
+    for document in documents:
+        chunks.extend(splitter_object.split_text(document.page_content))
+    return chunks
+
+extracted_documents=load_documents()
+#print(extracted_documents[2:5])
+chunks=split(extracted_documents)
+for i,_ in enumerate(chunks):
+    print(f"chunk[{i}](size: {len(chunks[i])}): {chunks[i]} \n")
