8547530a8f227df1fbeb633d49b326668d45291c
sumpumm <samarpan69.piya@gmail.com>
2024-12-23 16:18:59 +0545

    upload endpoint added

---
+++ pydantic_models.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+
+class Order(BaseModel):
+    name: str
+    items: int
+    
--- try.py
+++ try.py
@@ -1,2 +1,21 @@
-import pdfplumber
+from fastapi import FastAPI
+from pydantic_models import *
 
+#initiliaze the api
+app=FastAPI()
+
+@app.get("/home")
+async def home_endpoint():
+    return {"message":"successful"}
+
+@app.get("/greet")
+async def greet_endpoint(name: str = 'Samarpan'):
+    return {"message":f"hello, {name}"}
+
+@app.post("/order")
+async def order_endpoint(name: str ,quantity: int):
+    return {"message":f"the order of {quantity} {name} has been placed"}
+
+@app.post("/orderPyd")
+async def order_pydantic_endpoint(order: Order):
+    return {"message":f"the order of {order.items} {order.name} has been placed"}
